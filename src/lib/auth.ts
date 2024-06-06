462f5dfbf04eff70eb0963eceb266326aa77833c
aelys <aelysch@proton.me>
2024-06-06 00:22:45 -0300

    chore: Add authentication configuration for GitHub OAuth

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,17 +1,23 @@
 import { NextAuthOptions } from "next-auth"
 import CredentialsProvider from "next-auth/providers/credentials"
+import GithubProvider from "next-auth/providers/github"
 import { PrismaAdapter } from "@auth/prisma-adapter"
 // import { PrismaClient } from "@prisma/client"
+import { v4 as uuidv4 } from 'uuid';
 
 
 import { db as prisma } from "@/lib/db"
-import Github from "next-auth/providers/github"
 import { randomUUID } from "crypto"
+
 
 export const authOptions: NextAuthOptions = {
 
     adapter: PrismaAdapter(prisma as any),
     providers: [
+        GithubProvider({
+            clientId: process.env.GITHUB_ID!,
+            clientSecret: process.env.GITHUB_SECRET!
+        }),
 
         CredentialsProvider({
             name: 'Credentials',
@@ -21,9 +27,10 @@ export const authOptions: NextAuthOptions = {
                 name: { label: "Name", type: "text" }
             },
             async authorize(credentials) {
-                if (credentials) {
+                if (credentials?.email) {
                     console.log(credentials);
-                    const user = { id: randomUUID(), name: credentials.name, email: credentials.email, password: credentials.password };
+                    const user = { id: uuidv4(), name: credentials.name, email: credentials.email, password: credentials.password };
+
                     return user;
                 }
                 else {
